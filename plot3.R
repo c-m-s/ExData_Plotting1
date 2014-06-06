c4f438a2de21045cf9d3e25294c311fd9b250add
c-m-s <cmsmos@gmail.com>
2014-06-06 13:44:38 -0700

    Adding plot1:4.R and plot1:4.png files for assignment.

---
+++ plot3.R
@@ -0,0 +1,83 @@
+########################################################################################
+##
+## Date 2014-06-04
+## Written by Carl Smith for Exploratory Data Analysis class
+## 
+## Software used to test and run script:
+## MacOS X 10.9.3
+## R version 3.1.0 (2014-04-10) -- "Spring Dance"
+## RStudio Version 0.98.507
+##
+########################################################################################
+
+plot3 <- function() {
+        
+        ## Your code file should include code for reading the data so that the plot can be
+        ## fully reproduced. 
+        
+        # Check to see if the household_power_consumption.txt file is in the working directory.
+        if(!file.exists("./household_power_consumption.txt")) {
+                
+                ## Check if the "exdata data household_power_consumption.zip" file is around
+                ## and if not, then download it.
+                
+                if(!file.exists("./household_power_consumption.zip")) {
+                        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+                        download.file(fileURL, destfile="./household_power_consumption.zip",
+                                      method="curl",mode="wb")
+                        dataDownloaded <- date()
+                        print(paste("Downloaded electric power consumption zip file on", dataDownloaded))
+                }
+                
+                # Unzip the household_power_consumption.txt so it can be read and used to reproduce the plot.
+                # R manual recommends on MacOS to call unzip with full path.
+                unzip("./household_power_consumption.zip", unzip="/usr/bin/unzip",exdir = "./")        
+        }
+        
+        
+        
+        
+        # Make a date class to use to format the date string when read into read.table 
+        setClass('myDate')
+        setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y"))
+        
+        
+        powerdata <- read.table("./household_power_consumption.txt", 
+                                header=TRUE, 
+                                stringsAsFactors=FALSE, 
+                                sep=";",
+                                colClasses = c("myDate", "character", "numeric", "numeric", "numeric",
+                                               "numeric","numeric", "numeric", "numeric"),
+                                na.strings = "?")
+        
+        ## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+        
+        # Pull out all of the rows of data from 2007-02-01 and 2007-02-02.
+        powerdata2days <- powerdata[powerdata$Date == as.Date("2007-02-01") | powerdata$Date == as.Date("2007-02-02"),]
+        
+        # Add a new column that combines the date field and the time field to use later in plots.
+        powerdata2days$datetime <- strptime(paste(powerdata2days$Date, powerdata2days$Time), "%Y-%m-%d %H:%M:%S")
+        
+        
+        
+        
+        
+        ## For each plot you should
+        ##   - Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+        ##   - Name each of the plot files as plot1.png, plot2.png, etc.
+        
+        # general plot setup
+        png(file="plot3.png", width = 480, height = 480, units = "px", bg = "transparent")
+
+        # generate the plot into the open png graphics device
+        plot(powerdata2days$datetime, powerdata2days$Sub_metering_1, type="l", 
+             xlab="", ylab="Energy sub metering")
+        lines(powerdata2days$datetime, powerdata2days$Sub_metering_2, col="red")
+        lines(powerdata2days$datetime, powerdata2days$Sub_metering_3, col="blue")
+        legend("topright", lty=1, col=c("black", "red", "blue"), 
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        
+        # close the png graphics device
+        dev.off()
+        
+}
