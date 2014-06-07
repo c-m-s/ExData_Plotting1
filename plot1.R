84d6205b3574b03665edc2f1138e0418f4e124de
c-m-s <cmsmos@gmail.com>
2014-06-07 09:02:07 -0700

    cleaned up code then remade plots

--- plot1.R
+++ plot1.R
@@ -16,22 +16,21 @@ plot1 <- function() {
         ## fully reproduced. 
         
         # Check to see if the household_power_consumption.txt file is in the working directory.
-        if(!file.exists("./household_power_consumption.txt")) {
+        if(!file.exists("household_power_consumption.txt")) {
                 
                 ## Check if the "exdata data household_power_consumption.zip" file is around
                 ## and if not, then download it.
                 
-                if(!file.exists("./household_power_consumption.zip")) {
+                if(!file.exists("household_power_consumption.zip")) {
                         fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-                        download.file(fileURL, destfile="./household_power_consumption.zip",
+                        download.file(fileURL, destfile="household_power_consumption.zip",
                                       method="curl",mode="wb")
                         dataDownloaded <- date()
                         print(paste("Downloaded electric power consumption zip file on", dataDownloaded))
                 }
-        
-        # Unzip the household_power_consumption.txt so it can be read and used to reproduce the plot.
-        # R manual recommends on MacOS to call unzip with full path.
-        unzip("./household_power_consumption.zip", unzip="/usr/bin/unzip",exdir = "./")        
+                
+                # Unzip the household_power_consumption.txt so it can be read and used to reproduce the plot.
+                unzip("household_power_consumption.zip")        
         }
         
         
@@ -40,9 +39,9 @@ plot1 <- function() {
         # Make a date class to use to format the date string when read into read.table 
         setClass('myDate')
         setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y"))
-
-
-        powerdata <- read.table("./household_power_consumption.txt", 
+        
+        
+        powerdata <- read.table("household_power_consumption.txt", 
                                 header=TRUE, 
                                 stringsAsFactors=FALSE, 
                                 sep=";",
@@ -53,7 +52,7 @@ plot1 <- function() {
         ## We will only be using data from the dates 2007-02-01 and 2007-02-02.
         
         # Pull out all of the rows of data from 2007-02-01 and 2007-02-02.
-        powerdata2days <- powerdata[powerdata$Date == as.Date("2007-02-01") | powerdata$Date == as.Date("2007-02-02"),]
+        powerdata2days <- powerdata[powerdata$Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")),]
         
         # Add a new column that combines the date field and the time field to use later in plots.
         powerdata2days$datetime <- strptime(paste(powerdata2days$Date, powerdata2days$Time), "%Y-%m-%d %H:%M:%S")
